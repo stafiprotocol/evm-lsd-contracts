c4fda79a59bc14e0834a11891134a7dbb44f913d
tpkeeper <tpkeeper.me@gmail.com>
2024-02-21 17:26:13 +0800

    fmt

--- test/bnb/StakeManager.t.sol
+++ test/bnb/StakeManager.t.sol
@@ -2,31 +2,31 @@
 pragma solidity 0.8.19;
 
 import {Test, console2} from "forge-std/Test.sol";
-import { StakePoolManager } from "../../contracts/base/StakePoolManager.sol";
-import { StakeManager } from "../../contracts/bnb/StakeManager.sol";
-import { StakePool } from "../../contracts/bnb/StakePool.sol";
-import { IStaking } from "../../contracts/bnb/interfaces/IStaking.sol";
-import { LsdToken } from "../../contracts/LsdToken.sol";
-import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {StakePoolManager} from "../../contracts/base/StakePoolManager.sol";
+import {StakeManager} from "../../contracts/bnb/StakeManager.sol";
+import {StakePool} from "../../contracts/bnb/StakePool.sol";
+import {IStaking} from "../../contracts/bnb/interfaces/IStaking.sol";
+import {LsdToken} from "../../contracts/LsdToken.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 contract MockBnbStaking is IStaking {
-    mapping (address => mapping (address => uint256)) delegated;
-    mapping (address => uint256) totalDelegatedOf;
+    mapping(address => mapping(address => uint256)) delegated;
+    mapping(address => uint256) totalDelegatedOf;
 
-    function delegate(address validator, uint256 amount) external virtual payable {
+    function delegate(address validator, uint256 amount) external payable virtual {
         delegated[msg.sender][validator] = amount;
         totalDelegatedOf[msg.sender] += amount;
     }
 
-    function undelegate(address validator, uint256 amount) external virtual payable {}
+    function undelegate(address validator, uint256 amount) external payable virtual {}
 
-    function redelegate(address validatorSrc, address validatorDst, uint256 amount) external virtual payable {}
+    function redelegate(address validatorSrc, address validatorDst, uint256 amount) external payable virtual {}
 
-    function claimReward() external virtual pure returns (uint256) {
+    function claimReward() external pure virtual returns (uint256) {
         return 0;
     }
 
-    function claimUndelegated() external virtual pure returns (uint256) {
+    function claimUndelegated() external pure virtual returns (uint256) {
         return 0;
     }
 
@@ -34,46 +34,47 @@ contract MockBnbStaking is IStaking {
         return delegated[delegator][validator];
     }
 
-    function getTotalDelegated(address delegator) external virtual view returns (uint256) {
+    function getTotalDelegated(address delegator) external view virtual returns (uint256) {
         return totalDelegatedOf[delegator];
     }
 
-    function getDistributedReward(address delegator) external virtual pure returns (uint256) {
+    function getDistributedReward(address delegator) external pure virtual returns (uint256) {
         delegator;
         return 0;
     }
 
-    function getPendingRedelegateTime(
-        address delegator,
-        address valSrc,
-        address valDst
-    ) external virtual pure returns (uint256) {
+    function getPendingRedelegateTime(address delegator, address valSrc, address valDst)
+        external
+        pure
+        virtual
+        returns (uint256)
+    {
         delegator;
         valSrc;
         valDst;
         return 0;
     }
 
-    function getUndelegated(address delegator) external virtual pure returns (uint256) {
+    function getUndelegated(address delegator) external pure virtual returns (uint256) {
         delegator;
         return 0;
     }
 
-    function getPendingUndelegateTime(address delegator, address validator) external virtual pure returns (uint256) {
+    function getPendingUndelegateTime(address delegator, address validator) external pure virtual returns (uint256) {
         delegator;
         validator;
         return 0;
     }
 
-    function getRelayerFee() external virtual pure returns (uint256) {
+    function getRelayerFee() external pure virtual returns (uint256) {
         return 16e14;
     }
 
-    function getMinDelegation() external virtual pure returns (uint256) {
+    function getMinDelegation() external pure virtual returns (uint256) {
         return 1e18;
     }
 
-    function getRequestInFly(address delegator) external virtual pure returns (uint256[3] memory) {
+    function getRequestInFly(address delegator) external pure virtual returns (uint256[3] memory) {
         delegator;
         return [uint256(0), uint256(0), uint256(0)];
     }
@@ -117,23 +118,24 @@ contract StakeManagerTest is Test {
     event Stake(address staker, address poolAddress, uint256 tokenAmount, uint256 lsdTokenAmount);
     event Settle(uint256 indexed era, address indexed pool);
     event Delegate(address pool, address validator, uint256 amount);
+
     error OutOfFund();
     error NotVoter();
     error ZeroWithdrawAmount();
+
     function test_Stake() public {
         uint256 stakeAmount = 1e18;
         vm.expectEmit(false, false, false, true);
         emit Stake(address(this), address(pool), stakeAmount, stakeAmount);
-        manager.stake{value: stakeAmount+pool.getRelayerFee()}(stakeAmount);
-
+        manager.stake{value: stakeAmount + pool.getRelayerFee()}(stakeAmount);
 
         assertEq(lsdToken.balanceOf(address(this)), stakeAmount);
         assertEq(address(pool).balance, stakeAmount);
 
-        StakePoolManager.PoolInfo memory info ;
+        StakePoolManager.PoolInfo memory info;
         {
             (uint256 a, uint256 b, uint256 c, uint256 d) = manager.poolInfoOf(address(pool));
-            info = StakePoolManager.PoolInfo(a,b,c,d);
+            info = StakePoolManager.PoolInfo(a, b, c, d);
         }
         assertEq(info.era, 0);
         assertEq(info.bond, stakeAmount);
@@ -145,7 +147,7 @@ contract StakeManagerTest is Test {
         manager.settle(address(pool));
 
         // transfer relayer fee to pool for delegation
-        (bool success, ) = address(pool).call{value: 1e20}("");
+        (bool success,) = address(pool).call{value: 1e20}("");
         // (bool success, ) = address(pool).call{value: pool.getRelayerFee()}("");
         assertEq(success, true);
 
@@ -170,7 +172,7 @@ contract StakeManagerTest is Test {
         assertEq(manager.latestEra(), 0);
 
         vm.prank(voters[1]);
-        vm.warp(block.timestamp+86400);
+        vm.warp(block.timestamp + 86400);
         manager.newEra(poolList, rewardList, latestTimestampList);
         assertEq(manager.latestEra(), 1);
 
